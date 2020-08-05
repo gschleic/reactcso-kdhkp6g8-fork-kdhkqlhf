60f6aa785a400128a0435dbfb8eb13634908426a
Glenn Schleicher <gschleic@gmail.com>
2020-08-05 16:21:15 +0000

    First commit

---
+++ src/index.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MyCounter from "./index.js";
+
+export default {
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const story1 = () => <MyCounter></MyCounter>;
